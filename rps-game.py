e079037fb1808d48d5fe3e579954b621726880b7
Tejas Shah <tejashah88@gmail.com>
2023-06-05 02:38:50 -0700

    Update rps-game.py
    Added comments

--- rps-game.py
+++ rps-game.py
@@ -1,30 +1,37 @@
 import random
 
+# List of valid choices for the game
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
 
+# What to do when the game results in a tie
 def tie_game():
     print("It's a tie! :/")
 
 
+# What to do when the game results in the player winning
 def player_wins():
     print("You win! :)")
 
 
+# What to do when the game results in the computer winning
 def computer_wins():
     print("The computer wins! :(")
 
 
+# Get the choice from the player
 def ask_player_for_choice():
     choice = input('What do you choose (rock, paper, scissors): ').lower()
     return choice
 
 
+# Get the choice from the computer
 def ask_computer_for_choice():
     choice = random.choice(VALID_CHOICES)
     return choice
 
 
+# Determine who won based on both the player's and computer's choices
 def determine_winner(player_choice, computer_choice):
     if player_choice == computer_choice:
         tie_game()
@@ -48,7 +55,8 @@ def determine_winner(player_choice, computer_choice):
             computer_wins()
 
 
-print("Let's play a game of Rock, Paper Scissors!")
+# Main game loop that runs forever
+print("Let's play a game of Rock, Paper, Scissors!")
 while True:
     player_choice = ask_player_for_choice()
     while player_choice not in VALID_CHOICES:
