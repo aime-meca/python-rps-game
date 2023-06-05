2d8b9409872109be21aa0bc799360c577c747810
Tejas Shah <tejashah88@gmail.com>
2023-06-05 02:39:14 -0700

    Added skeleton template for RPS game

---
+++ rps-game-incomplete.py
@@ -0,0 +1,47 @@
+import random
+
+# List of valid choices for the game [TODO]
+VALID_CHOICES = []
+
+
+# What to do when the game results in a tie [TODO]
+def tie_game():
+    pass
+
+
+# What to do when the game results in the player winning [TODO]
+def player_wins():
+    pass
+
+
+# What to do when the game results in the computer winning [TODO]
+def computer_wins():
+    pass
+
+
+# Get the choice from the player [TODO]
+def ask_player_for_choice():
+    pass
+
+
+# Get the choice from the computer [TODO]
+def ask_computer_for_choice():
+    pass
+
+
+# Determine who won based on both the player's and computer's choices [TODO]
+def determine_winner(player_choice, computer_choice):
+    pass
+
+
+# Main game loop that runs forever [TODO]
+print("Let's play a game of Rock, Paper, Scissors!")
+while True:
+    """
+    Pseudocode (within loop):
+    1. Ask for the player's choice
+    2. Ask for the computer's choice
+    3. Determine the winner and print the result
+    """
+    
+    pass
